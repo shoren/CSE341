f4a8be5fbb02dd0b756c878514b55ccc3e89e03b
shoren <65180338+shoren@users.noreply.github.com>
2022-09-23 23:33:16 -0600

    working localhost

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,4 @@
-const { Router } = require('express');
-const express = require('express');
 const routes = require ('express').Router();
-
-//Router.use('contacts', require('./contacts'))
-
 
 routes.get('/',(req, res) =>{
     res.send('Caleb Brownie');
