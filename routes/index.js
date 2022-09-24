53680571db82e0f1d4d6501b6454515cc936328d
shoren <65180338+shoren@users.noreply.github.com>
2022-09-23 21:33:04 -0600

    getting set up

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
-const routes = require ('express').Router();
+const { Router } = require('express');
+const express = require('express');
+const router = require ('express').Router();
 
-routes.get('/',(req, res) =>{
-    res.send('Caleb Brownie');
-});
 
-module.exports = routes;
+//Get the file that we are looking for
+Router.use('contacts', require('./contacts'))
+
+
+// routes.get('/',(req, res) =>{
+//     res.send('Caleb Brownie');
+// });
+
+module.exports = router;
