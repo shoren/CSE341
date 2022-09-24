5fa83999ef148eb0ad2aa2298cbdc1ae0853d744
shoren <65180338+shoren@users.noreply.github.com>
2022-09-24 01:18:11 -0600

    gave up went to bed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,19 @@
-const routes = require ('express').Router();
+// const routes = require ('express').Router();
+// //const app = express()
+// const express = require ('express')
+// const app = express()
 
-routes.get('/',(req, res) =>{
-    res.send('Caleb Brownie');
-});
+// // app.get('/', require('../../frontend/index.html'))
 
-module.exports = routes;
+// routes.get('/',(req, res) =>{
+//     res.send('Caleb Brownie');
+// });
+
+// module.exports = routes;
+
+//This was added 
+const express = require('express');
+const router = express.Router();
+
+router.use('/contacts',require('./contacts'))
+module.exports = router;
