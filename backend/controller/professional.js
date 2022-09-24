4ad9c52f6eca0f6a53135e27f8c1f3c3802698f2
shoren <65180338+shoren@users.noreply.github.com>
2022-09-23 21:59:09 -0600

    added the frontend files

--- backend/controller/professional.js
+++ backend/controller/professional.js
@@ -8,3 +8,5 @@ const router = express.Router();
 router.get('/', professionalController.getData);
 // localhost:8080/professional/
 module.exports = router;
+
+//this might be wrong i have no idea what is going on here
