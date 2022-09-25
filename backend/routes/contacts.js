e396142cefc00bc1b30fb2b5d7f6482ccdb87b6b
shoren <65180338+shoren@users.noreply.github.com>
2022-09-24 22:17:35 -0600

    path change

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // const routes = require ('express').Router();
 const router = express.Router();
-const contactsController = require('../controller/contacts');
+const contactsController = require('../controller/contacts.controller');
 
 // Get all of the documents in the contacts collection
 router.get('/', contactsController.getAll);
