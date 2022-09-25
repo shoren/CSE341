dde2a1b60456d037e488318a4073747b4f488136
shoren <65180338+shoren@users.noreply.github.com>
2022-09-24 21:42:45 -0600

    removed extra files

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require ('express')
-const app = express()
-const port = process.env.PORT || 3000
-
-app.get('/', require('./backend/routes'))
-
-app.listen(port,() => {
-    console.log(`Example app listening on port ${port}`)
-})
