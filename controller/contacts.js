ca77d737a51b6505e6f0904687877e73fa6ad9db
shoren <65180338+shoren@users.noreply.github.com>
2022-10-03 20:20:07 -0600

    edit contact testing

--- controller/contacts.js
+++ controller/contacts.js
@@ -1,3 +1,4 @@
+const { response } = require('express');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 // GET
@@ -12,18 +13,14 @@ const getAll = async (req, res, next) => { //Have to put the name of the Databas
   // GET
   const getSingle = async (req, res, next) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-      .getDb()
-      .db('Test')
-      .collection('contacts')
-      .find({ _id: userId });
+    const result = await mongodb.getDb().db('Test').collection('contacts').find({ _id: userId }); //Find
     result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists[0]);
     });
   };
 
-  //POST
+  //POST working!
   const createContact = async (req, res, next) => {
     const contact = {
       firstName: req.body.firstName,
@@ -32,7 +29,7 @@ const getAll = async (req, res, next) => { //Have to put the name of the Databas
       favoriteColor: req.body.favoriteColor,
       birthday: req.body.birthday,
     };
-    const result = await mongodb.getDb().db('Test').collection('contacts').insertOne(contact);
+    const result = await mongodb.getDb().db('Test').collection('contacts').insertOne(contact); //insertOne
     if(result.acknowledged){
       req.status(201).json(response);
     }
@@ -41,29 +38,28 @@ const getAll = async (req, res, next) => { //Have to put the name of the Databas
   // PUT
   const updateContact = async (req, res, next) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-      .getDb()
-      .db('Test')
-      .collection('contacts')
-      .find({ _id: userId });
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(204).json(lists[0]);
-    });
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday,
+    };
+
+    const result = await mongodb.getDb().db('Test').collection('contacts').replaceOne({ _id: userId },contact); //replaceOne
+    if(response.acknowledged){
+      req.status(204).json(response);
+    }
   };
 
   // DELETE
   const deleteContact = async (req, res, next) => {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb
-      .getDb()
-      .db('Test')
-      .collection('contacts')
-      .find({ _id: userId });
+    const result = await mongodb.getDb().db('Test').collection('contacts').remove({ _id: userId }); // Remove
     result.toArray().then((lists) => {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).json(lists[0]);
     });
   };
   
-  module.exports = { getAll, getSingle,createContact,updateContact,deleteContact };
+  module.exports = { getAll,getSingle,createContact,updateContact,deleteContact };
