dc41bfc359dff9d5c501b9746df2917985a56179
shoren <65180338+shoren@users.noreply.github.com>
2022-10-08 16:20:56 -0600

    host path

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/contacts',require('./contacts'))
+router.use('/contacts',require('./contacts'));
+router.use('/api-docs', require('./docs'));
 module.exports = router;
