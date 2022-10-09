23709f744b6c2c0cf30bfb026de80a16dd39abdb
shoren <65180338+shoren@users.noreply.github.com>
2022-10-08 22:09:14 -0600

    final touches

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Temple API',
   },
-  host: 'localhost:8080',
-  schemes: ['http','https'],
+  host: 'appConfig.host',
+  schemes: [appConfig.protocol],
 };
 
 const outputFile = './swagger.json';
