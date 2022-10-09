8c202271c55d063a9ebb89ed4e2b7ccdfe9c63cd
shoren <65180338+shoren@users.noreply.github.com>
2022-10-08 22:07:43 -0600

    coros

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const openCors = require("../middleware/openCors");
 router.use('/contacts',require('./contacts'));
 router.use('/api-docs', require('./docs'));
+router.use([openCors, express.json()]);
+
 module.exports = router;
